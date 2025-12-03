578c6eeda70dc20bfe91d82a87d5d86d191daf25
swasthikg73 <swasthikg32@gmail.com>
2025-12-03 14:31:17 +0530

    Orders Page on Admin

--- admin/src/pages/Orders/Orders.jsx
+++ admin/src/pages/Orders/Orders.jsx
@@ -1,12 +1,75 @@
 import React from "react";
 import "./Orders.css";
+import { useState } from "react";
+import axios from "axios";
+import { useEffect } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import { assets } from "../../assets/assets";
 
 const Orders = ({ url }) => {
+  const [orders, setOrders] = useState([]);
+
+  const fetchAllOrders = async () => {
+    const res = await axios.get(url + "/api/order/list");
+    // console.log(res.data.data);
+
+    if (res.data.success) {
+      setOrders(res.data.data);
+    } else {
+      toast.error("Error");
+      console.log("Error");
+    }
+  };
+  useEffect(() => {
+    fetchAllOrders();
+  }, []);
   return (
-    <div className="order add">
-      <h3>Order Page</h3>
-      <div className="order-list"></div>
-    </div>
+    <>
+      <ToastContainer />
+      <div className="order add">
+        <h3>Order Page</h3>
+        <div className="order-list">
+          {orders.map((order, index) => (
+            <div key={index} className="order-item">
+              <img src={assets.parcel_icon} alt="" />
+              <div>
+                <p className="order-item-food">
+                  {order.items.map((item, index) => {
+                    if (index === order.items.length - 1) {
+                      return item.name + " x " + item.quantity;
+                    }
+                    return item.name + " x " + item.quantity + ", ";
+                  })}
+                </p>
+                <p className="order-item-name">
+                  {order.address.firstName + " " + order.address.lastName}
+                </p>
+                <div className="order-item-address">
+                  <p>{order.address.street + ","}</p>
+                  <p>
+                    {order.address.city +
+                      ", " +
+                      order.address.state +
+                      ", " +
+                      order.address.country +
+                      ", " +
+                      order.address.zipcode}
+                  </p>
+                </div>
+                <p className="order-item-phone">{order.address.phone}</p>
+              </div>
+              <p>Items : {order.items.length}</p>
+              <p>${order.amount}</p>
+              <select name="" id="">
+                <option value="food Processing">Food Processing</option>
+                <option value="Out for delivery">Out for Delivery</option>
+                <option value="Delivery">Delivery</option>
+              </select>
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
   );
 };
 
--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -90,8 +90,15 @@ const userOrders = async (req, res) => {
 
 const listOrders = async (req, res) => {
   try {
-    const orders = await orderModel.findAll();
-  } catch (error) {}
+    const orders = await orderModel.find({});
+    res.json({
+      success: true,
+      data: orders,
+    });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: "Error" });
+  }
 };
 
 export { placeOrder, verifyOrder, userOrders, listOrders };
