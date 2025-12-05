aff449d54979daadeeaa458ed54fab301c8fabf2
swasthikg73 <swasthikg32@gmail.com>
2025-12-05 12:52:35 +0530

    Update Order functionality

--- admin/src/components/Navbar/Navbar.jsx
+++ admin/src/components/Navbar/Navbar.jsx
@@ -5,7 +5,7 @@ import { assets } from "../../assets/assets";
 const Navbar = () => {
   return (
     <div className="navbar">
-      <img className="logo" src={assets.logo} alt="logo" />
+      <img className="logo" src={assets.logo2} alt="logo" />
       <img className="profile" src={assets.profile_image} alt="profile" />
     </div>
   );
