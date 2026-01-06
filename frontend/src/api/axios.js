891db6f8a5a48f4c8afb0d89e09305a39a9ee1f8
swasthikg73 <swasthikg32@gmail.com>
2026-01-06 15:47:51 +0530

    Base URL updated

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-// const base_url = "https://cravex-a-food-app-backend.onrender.com";
-const base_url = "http://localhost:3200";
+const base_url = "https://cravex-a-food-app-backend.onrender.com";
+//const base_url = "http://localhost:3200";
 
 const axiosPrivate = axios.create({
   baseURL: base_url,
